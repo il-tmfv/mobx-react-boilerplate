5c05a7a77424d7123fa2fba6ffe79cc3bb88bd53
Ilya Timofeev <il.tmfv@gmail.com>
2017-03-01 00:06:55 +0300

    Optimize building

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,37 @@
 var path = require('path');
 var webpack = require('webpack');
 
+const constants = {
+  TEST: process.env.NODE_ENV === 'test',
+  HOT: process.env.HOT === 'true',
+  LOCAL: process.env.NODE_ENV === 'local' || process.env.NODE_ENV === 'api',
+  STAGE: process.env.NODE_ENV === 'stage',
+  DEVELOP: process.env.NODE_ENV === 'develop',
+  PRODUCTION: process.env.NODE_ENV === 'production',
+};
+
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    }
+  }),
+];
+
+if (constants.PRODUCTION || constants.STAGE) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin());
+  plugins.push(new webpack.LoaderOptionsPlugin({
+    minimize: true
+  }));
+}
+
+if (constants.LOCAL) {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+}
+
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'inline-source-map',
+  target: 'web',
   entry: [
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:3000',
@@ -10,13 +39,11 @@ module.exports = {
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: plugins,
   resolve: {
     extensions: ['.js', '.jsx'],
     modules: [
